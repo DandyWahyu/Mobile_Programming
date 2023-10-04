00c7a31e772c0de167159d39a7e9fa8ce6ea5b99
DandyWahyu <2141720002@student.polinema.ac.id>
2023-10-04 20:56:35 +0700

    hello_world

--- dart_application_2/bin/praktikum_5.dart
+++ dart_application_2/bin/praktikum_5.dart
@@ -3,15 +3,16 @@ void main() {
   print(record);
 
   (int, int) tukar((int, int) record) {
-  var (a, b) = record;
-  return (b, a);
+    var (a, b) = record;
+    return (b, a);
   }
+
   print(tukar((1, 2)));
 
   // Record type annotation in a variable declaration:
   (String, int) mahasiswa = ('Dandy Wahyu Syahputra', 2141720002);
   print(mahasiswa);
-  
+
   var mahasiswa2 = ('Dandy Wahyu Syahputra', a: 2141720002, b: true, 'last');
 
   print(mahasiswa2.$1); // Prints 'Dandy Wahyu Syahputra'
